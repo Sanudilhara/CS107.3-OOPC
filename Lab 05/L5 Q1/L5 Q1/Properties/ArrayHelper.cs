94580939e58e7838156b8e0174738d0b9c931c2a
GAS Dilhara <136427386+sanudilhara@users.noreply.github.com>
2023-07-29 22:43:14 +0530

    Add files via upload

---
+++ Lab 05/L5 Q1/L5 Q1/Properties/ArrayHelper.cs
@@ -0,0 +1,30 @@
+﻿using ArrayOperations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayHelper arrayHelper = new ArrayHelper(10);
+        arrayHelper.InputValues();
+
+        int minValue = arrayHelper.GetMinimumValue();
+        int maxValue = arrayHelper.GetMaximumValue();
+        double averageValue = arrayHelper.GetAverageValue();
+
+        Console.WriteLine($"Minimum value: {minValue}");
+        Console.WriteLine($"Maximum value: {maxValue}");
+        Console.WriteLine($"Average value: {averageValue:F2}");
+
+        arrayHelper.ReverseOrder();
+        Console.WriteLine("Reversed order of values:");
+        foreach (int num in ArrayHelper)
+        {
+            Console.Write($"{num} ");
+        }
+    }
+}
