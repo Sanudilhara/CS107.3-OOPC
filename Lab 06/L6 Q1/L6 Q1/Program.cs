631c31c5b4806d4ee5b702b6285a56bd6911b5e2
GAS Dilhara <136427386+sanudilhara@users.noreply.github.com>
2023-07-29 22:44:53 +0530

    Add files via upload

---
+++ Lab 06/L6 Q1/L6 Q1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L6_Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array: ");
+            int size;
+            while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer value.");
+                Console.Write("Enter the size of the array: ");
+            }
+
+            ArrayHelper arrayHelper = new ArrayHelper(size);
+            arrayHelper.InputValues();
+            arrayHelper.PrintArray();
+        }
+    }
+}
---
+++ Lab 06/L6 Q1/L6 Q1/Properties/ArrayHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayInputWithZeros
+    {
+        class ArrayHelper
+        {
+            private int[] array;
+
+            public ArrayHelper(int size)
+            {
+                array = new int[size * 2]; 
+            }
+
+            public void InputValues()
+            {
+                Console.WriteLine($"Enter {array.Length / 2} integer values, each followed by a value 0:");
+                for (int i = 0; i < array.Length; i += 2)
+                {
+                    int input;
+                    while (!int.TryParse(Console.ReadLine(), out input))
+                    {
+                        Console.WriteLine("Invalid input. Please enter an integer value.");
+                    }
+                    array[i] = input;
+                    array[i + 1] = 0;
+                }
+            }
+
+            public void PrintArray()
+            {
+                Console.WriteLine("Values inside the array:");
+                foreach (int num in array)
+                {
+                    Console.Write($"{num} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
